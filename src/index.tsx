22f7228b36087ca191b7a153adc0ef694c0ada47
Ram kumar <webbieram@outlook.com>
2022-07-23 01:42:59 +0530

    renamed readme file name

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(container);
 root.render(<App/>)
